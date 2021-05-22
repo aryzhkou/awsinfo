c2d9dda8fdc540ca271e02dee8081b92f6f017e9
Aliaksei Ryzhkou <a.ryzhkou@gmail.com>
2021-05-22 18:44:36 +0300

    model changes

--- src/main/java/com/barbariania/awsinfo/dto/FileMetadata.java
+++ src/main/java/com/barbariania/awsinfo/dto/FileMetadata.java
@@ -2,18 +2,26 @@ package com.barbariania.awsinfo.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
 public class FileMetadata {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
-  private Instant lastUpdateTime;
+  private LocalDateTime lastUpdateTime;
   private String name;
   private long size; // bytes
   private String extension;
+  private String link;
 }
