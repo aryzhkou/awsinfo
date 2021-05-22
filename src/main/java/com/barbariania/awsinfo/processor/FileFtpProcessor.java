f279d1d122d5d28f95e8be3399bc7bd717282aaa
Aliaksei Ryzhkou <a.ryzhkou@gmail.com>
2021-05-22 18:45:00 +0300

    File service

--- src/main/java/com/barbariania/awsinfo/processor/FileFtpProcessor.java
+++ src/main/java/com/barbariania/awsinfo/processor/FileFtpProcessor.java
@@ -1,12 +1,8 @@
 package com.barbariania.awsinfo.processor;
 
-import com.barbariania.awsinfo.dao.FileMetadataDao;
-import com.barbariania.awsinfo.dto.FileMetadata;
-import com.barbariania.awsinfo.exception.FileStorageException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,9 +20,11 @@ public class FileFtpProcessor implements FileProcessor {
   @Value("${app.upload.dir:${user.home}}")
   private String uploadDir;
 
-  public void upload(MultipartFile file) throws IOException {
+  public String upload(MultipartFile file) throws IOException {
     Path copyLocation = Paths.get(uploadDir
         + File.separator + StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename())));
     Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+
+    return copyLocation.toString();
   }
 }
--- src/main/java/com/barbariania/awsinfo/service/FileService.java
+++ src/main/java/com/barbariania/awsinfo/service/FileService.java
@@ -1,6 +1,6 @@
 package com.barbariania.awsinfo.service;
 
-import com.barbariania.awsinfo.dao.FileMetadataDao;
+import com.barbariania.awsinfo.dao.FileMetadataRepository;
 import com.barbariania.awsinfo.dto.FileMetadata;
 import com.barbariania.awsinfo.exception.FileStorageException;
 import com.barbariania.awsinfo.processor.FileProcessor;
@@ -9,21 +9,65 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
   private final FileProcessor fileProcessor;
-  private final FileMetadataDao fileMetadataDao;
+  private final FileMetadataRepository fileMetadataRepository;
 
   public FileMetadata upload(MultipartFile file) {
     try {
-      fileProcessor.upload(file);
-      return fileMetadataDao.save(file.getOriginalFilename(), file.getSize());
+      String filePath = fileProcessor.upload(file);
+
+      return save(file.getOriginalFilename(), file.getSize(), filePath);
     } catch (IOException e) {
       e.printStackTrace();
       //fixme delete from s3 if error response from db
       throw new FileStorageException("Could not store file " + file.getOriginalFilename());
     }
+  }
+
+  public MultipartFile download(String filename) {
+    return null; //todo fixme
+  }
+
+  public List<FileMetadata> getAllFilesMetadata() {
+    return fileMetadataRepository.findAll();
+  }
+
+  public FileMetadata getMetadataByFilename(String name) {
+    return name == null
+        ? fileMetadataRepository.findTopByOrderByIdDesc() //return latest
+        : fileMetadataRepository.findByName(name);
+  }
+
+  public void delete(String name) {
+    //todo implement
+  }
+
+  private FileMetadata save(String filename, long size, String path) {
+    Instant instantTime = Instant.now();
+    LocalDateTime utcDateTime = instantTime.atZone(ZoneOffset.UTC).toLocalDateTime();
+
+    FileMetadata fileMetadata = new FileMetadata();
+    fileMetadata.setName(filename);
+    fileMetadata.setSize(size);
+    fileMetadata.setExtension(getExtension(filename));
+    fileMetadata.setLastUpdateTime(utcDateTime);
+    fileMetadata.setLink(path);
+
+    fileMetadata = fileMetadataRepository.save(fileMetadata);
+
+    return fileMetadata;
+  }
+
+  private String getExtension(String filename) {
+    final int firstExtensionSymbolPosition = filename.lastIndexOf(".") + 1;
+    return firstExtensionSymbolPosition > 0 ? filename.substring(firstExtensionSymbolPosition) : null;
   }
 }
