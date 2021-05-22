cf0beb7f20230ec195bcbf3a44b3b5d4f0fd21a8
Aliaksei Ryzhkou <a.ryzhkou@gmail.com>
2021-05-22 18:54:40 +0300

    config for db

---
+++ src/main/java/com/barbariania/awsinfo/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.barbariania.awsinfo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+  @Value("${sql.driverClassName}")
+  private String driverClassName;
+  @Value("${sql.url}")
+  private String dbUrl;
+  @Value("${sql.schema}")
+  private String schema;
+  @Value("${sql.username}")
+  private String dbUsername;
+  @Value("${sql.password}")
+  private String dbPassword;
+
+  @Bean
+  public DriverManagerDataSource dataSource() throws ClassNotFoundException {
+    Class.forName(driverClassName);
+    DriverManagerDataSource dataSource = new DriverManagerDataSource(dbUrl, dbUsername, dbPassword);
+    dataSource.setDriverClassName(driverClassName);
+    return dataSource;
+  }
+}
